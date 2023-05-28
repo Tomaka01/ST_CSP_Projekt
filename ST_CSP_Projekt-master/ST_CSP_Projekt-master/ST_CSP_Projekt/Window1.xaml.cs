bd50425997bc52b63d1043b9cdd135808dcb16b0
CsPatrik2006 <csordaspatrik11@gmail.com>
2023-05-28 18:04:39 +0200

    Regisztracio_Design_Done

--- ST_CSP_Projekt-master/ST_CSP_Projekt-master/ST_CSP_Projekt/Window1.xaml.cs
+++ ST_CSP_Projekt-master/ST_CSP_Projekt-master/ST_CSP_Projekt/Window1.xaml.cs
@@ -142,5 +142,25 @@ namespace ST_CSP_Projekt
             abalksz.Show();
             Close();
         }
+
+        private void web_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://premontrei-keszthely.hu");
+        }
+
+        private void facebook_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/Premontrei.Keszthely");
+        }
+
+        private void insta_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.instagram.com/premontrei_kozepiskola/");
+        }
+
+        private void logo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Szeretünk Józsi Bácsi! ;)");
+        }
     }
 }
